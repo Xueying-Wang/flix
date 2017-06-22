4453da553e129ad7edbdcb2f5f2ddc26a11779f0
Xueying Wang <xueyingw@fb.com>
2017-06-22 11:23:59 -0700

    required tasks done: add movie feeds, a loading state, detail views and a pull-to-refresh function

---
+++ flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Xueying Wang on 6/21/17.
+//  Copyright © 2017 Xueying Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
