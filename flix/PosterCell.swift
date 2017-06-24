055d0935b1dfd452c03c59ec80d18bd6488b3f69
Xueying Wang <xueyingw@fb.com>
2017-06-23 18:01:26 -0700

    Add search bar, collection view, online trailers, etc.

---
+++ flix/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Xueying Wang on 6/23/17.
+//  Copyright © 2017 Xueying Wang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
---
+++ flix/TrailerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrailerViewController.swift
+//  flix
+//
+//  Created by Xueying Wang on 6/23/17.
+//  Copyright © 2017 Xueying Wang. All rights reserved.
+//
+
+import UIKit
+
+class TrailerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
